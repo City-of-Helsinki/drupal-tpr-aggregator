4419298179f8b4091354d1fda2f4a9de1b99d26c
tuutti <tuutti@iki.fi>
2021-10-17 07:42:38 +0300

    Update every 3 hours

--- src/Commands/AggregateTprServiceCommand.php
+++ src/Commands/AggregateTprServiceCommand.php
@@ -8,7 +8,7 @@ use App\Http\ServiceClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Violet\StreamingJsonEncoder\BufferJsonEncoder;
+use Violet\StreamingJsonEncoder\StreamJsonEncoder;
 
 final class AggregateTprServiceCommand extends Command
 {
@@ -21,19 +21,23 @@ final class AggregateTprServiceCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $encoder = (new BufferJsonEncoder(function () {
-            foreach ($this->serviceHttpClient->all() as $item) {
-                $service = [
-                    'id' => $item->id,
-                ];
-                foreach (['fi', 'en', 'sv'] as $language) {
-                    $service[$language] = $this->serviceHttpClient->get($item->id, $language) ?? [];
+        $encoder = (new StreamJsonEncoder(
+            function () {
+                foreach ($this->serviceHttpClient->all() as $item) {
+                    $serviceGroup = [
+                        'id' => $item['id'],
+                    ];
+                    foreach (['fi', 'en', 'sv'] as $language) {
+                        $serviceGroup[$language] = $this->serviceHttpClient->get($item['id'], $language) ?? [];
+                    }
+
+                    yield $serviceGroup;
                 }
-                yield $service;
             }
-        }))->setOptions(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        ))->setOptions(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
         $output->write($encoder->encode());
+
         return Command::SUCCESS;
     }
 }
