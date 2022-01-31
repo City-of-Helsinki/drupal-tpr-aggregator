b23925d5a8eda0ba012630c8ffec1a28365359f6
tuutti <tuutti@iki.fi>
2022-01-31 14:57:45 +0200

    Generate a list of units with all their services

--- console.php
+++ console.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Commands\AggregateTprServiceCommand;
+use App\Commands\AggregateUnitServices;
 use App\Http\ServiceClient;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Application;
@@ -12,4 +13,5 @@ use Symfony\Component\Console\Application;
 $serviceHttpClient = new ServiceClient(new Client(['base_uri' => ServiceClient::BASE_URI]));
 $application = new Application();
 $application->add(new AggregateTprServiceCommand($serviceHttpClient));
+$application->add(new AggregateUnitServices($serviceHttpClient));
 $application->run();
---
+++ src/Commands/AggregateUnitServices.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class AggregateUnitServices extends Command
+{
+    protected static $defaultName = 'app:aggregate-unit-services';
+
+    public function configure()
+    {
+        $this->addArgument(
+            'servicesFile',
+            InputArgument::REQUIRED,
+            'Previously generated services data file.'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $fileName = $input->getArgument('servicesFile');
+
+        if (!file_exists($fileName)) {
+            throw new \InvalidArgumentException(sprintf('Service file %s not found.', $fileName));
+        }
+        // Generate a list of units containing a list of services provided in
+        // that given unit.
+        $data = json_decode(file_get_contents($fileName), true);
+
+        $services = [];
+
+        foreach ($data as $item) {
+            foreach ($item['fi']['unit_ids'] ?? [] as $unitId) {
+                $services[$unitId]['unit_id'] = $unitId;
+                $services[$unitId]['services'][] = $item['fi']['id'];
+            }
+        }
+        $output->write(json_encode($services, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+        return Command::SUCCESS;
+    }
+}
