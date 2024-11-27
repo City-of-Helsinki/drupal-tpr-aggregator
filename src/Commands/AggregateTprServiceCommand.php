ef2ba32d464651ca43599bf9031584c37a3e4634
tuutti <tuutti@iki.fi>
2024-11-27 08:34:59 +0200

    Updates

--- src/Commands/AggregateTprServiceCommand.php
+++ src/Commands/AggregateTprServiceCommand.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace App\Commands;
 
 use App\Http\ServiceClient;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Violet\StreamingJsonEncoder\BufferJsonEncoder;
 
+#[AsCommand(name: 'app:aggregate-tpr-services')]
 final class AggregateTprServiceCommand extends Command
 {
-    protected static $defaultName = 'app:aggregate-tpr-services';
-
     public function __construct(private ServiceClient $serviceHttpClient)
     {
         parent::__construct();
